d43f02ecd6887c5768369710d02c45c6ed364ea0
Gustav Svensk <grulfen3@gmail.com>
2012-03-22 20:43:49 +0100

    Flyttat funktioner från sensor.c till sensor_ad.c

--- Kod/sensor/sensor.c
+++ Kod/sensor/sensor.c
@@ -3,108 +3,28 @@
  *
  *       Filename:  sensor.c
  *
- *    Description:  Sensorenhetens funktioner
+ *    Description:  Sensorenhetens huvudloop 
  *
  *        Version:  1.0
  *        Created:  2012-03-22 17:34:42
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  Gustav 
+ *         Author:  Tobias 
  *   Organization:  
  *
  * =====================================================================================
  */
+/*
+Koden ställer in de interna och externa muxarna för adomvandlaren.
+Omvandlingskedjan startas av en intern timer ca var 35 ms. (16Mhz).
+De digitala omvandlas till avstånd i convert_to_distance (behöver skrivas)
+*/
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "sensor_ad.h"
 
-
-/* 
- * ===  FUNCTION  ======================================================================
- *         Name:  calculate_distance_diff
- *  Description:  Beräknar skillnad i avstånd givet avståndssensorvärden
- * =====================================================================================
- */
-int calculate_distance_diff(unsigned char distance_left, unsigned char distance_right)
-{
-        int diff = distance_left - distance_right;
-        diff = diff >> 1;
-        return diff;
-}
-
-/* 
- * ===  FUNCTION  ======================================================================
- *         Name:  calculate_diff
- *  Description:  Input: 11bitar med linjesensorvärde (t ex 00001100000)
- *                Output: Värde som ska regleras på (positivt om roboten är till höger 
- *                och negativt om roboten är till vänster).
- * =====================================================================================
- */
-int calculate_diff(int value)
-{
-        int index = get_first_one(value);
-        printf("Index är %d\n", index);
-        if(index == -1){
-                printf("Inga ettor i datat\n");
-                return 0;
-        }
-        switch(index){
-                case 0 :
-                        return -128;
-                case 1:
-                        return -102;
-                case 2:
-                        return -78;
-                case 3:
-                        return -53;
-                case 4:
-                        return -25;
-                case 5:
-                        return 0;
-                case 6:
-                        return 25;
-                case 7:
-                        return 53;
-                case 8:
-                        return 78;
-                case 9:
-                        return 102;
-                case 10:
-                        return 127;
-                default:
-                        return 0;
-        }
-}
-
-/* 
- * ===  FUNCTION  ======================================================================
- *         Name:  get_first_one
- *  Description:  Funktion som hittar den första ettan från vänster.
- *                Input: value (11-bitar)
- *                Output: index på första ettan från vänster. -1 om ingen etta.
- * =====================================================================================
- */
-int get_first_one(int value)
-{
-        for(int i=0;i<11;i++){
-                if(value%2 == 1){
-                        return i; 
-                }
-                value = value >> 1;
-        }
-        return -1;
-}
-
-/*
-Skapad av: Tobias
-Datum: 19/3 2012
-
-Koden ställer in de interna och externa muxarna för adomvandlaren.
-Omvandlingskedjan startas av en intern timer ca var 35 ms. (16Mhz).
-De digitala omvandlas till avstånd i convert_to_distance (behöver skrivas)
-*/
 
 ISR(ADC_vect)
 {
--- Kod/sensor/sensor_ad.c
+++ Kod/sensor/sensor_ad.c
@@ -1,21 +1,16 @@
-/*
-Fyller en byte med data där bitarna visar 1 för svart och 0 för vit
-*/
-
 void create_line_array(int trunc_value, int vect_id);
 
 int truncate(unsigned char inbyte);
 
-/*
-Omvandlar digitalt värde till avstånd (trots sitt namn). Koden behöver skrivas.
-*/
 int convert_to_distance(unsigned char analog_distance);
 
-/*
-Styr muxar, startar ad och anropar omvandlingar
-*/
 
-
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  start_next_ad
+ *  Description:  Styr muxar, ad och anropar omvandlingar
+ * =====================================================================================
+ */
 void start_next_ad()
 {
 
@@ -92,6 +87,12 @@ void start_next_ad()
 }
 
 
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  create_line_array
+ *  Description:  Fyller två byte med data där bitarna visar 1 för svart och 0 för vit
+ * =====================================================================================
+ */
 void create_line_array(int trunc_value, int vect_id)
 {
 		if (vect_id==1){
@@ -108,15 +109,27 @@ void create_line_array(int trunc_value, int vect_id)
 		}
 }
 
-/*
-Denna funktion ska slå upp de digitala värdena i en taball och returnera
-vilket avstånd de motsvarar*/
-
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  convert_to_distance
+ *  Description:  Input: digital sensorvärde
+ *  		  Output: avstånd i cm
+ * =====================================================================================
+ */
 int convert_to_distance(unsigned char analog_distance)
 {
+		TODO
 		return 0;
 }
 
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  truncate
+ *  Description:  Trösklar invärdet
+ *  		  Input: en byte som ska trösklas
+ *  		  Output: 1 om byten är mindre än tröskelvärdet och 0 annars
+ * =====================================================================================
+ */
 int truncate(unsigned char inbyte)
 {
 		if(inbyte < level){
@@ -124,4 +137,80 @@ int truncate(unsigned char inbyte)
 		} else {
 				return 0;
 		}
+}
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  calculate_distance_diff
+ *  Description:  Beräknar skillnad i avstånd givet avståndssensorvärden
+ * =====================================================================================
+ */
+int calculate_distance_diff(unsigned char distance_left, unsigned char distance_right)
+{
+        int diff = distance_left - distance_right;
+        diff = diff >> 1;
+        return diff;
+}
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  calculate_diff
+ *  Description:  Input: 11bitar med linjesensorvärde (t ex 00001100000)
+ *                Output: Värde som ska regleras på (positivt om roboten är till höger 
+ *                och negativt om roboten är till vänster).
+ * =====================================================================================
+ */
+int calculate_diff(int value)
+{
+        int index = get_first_one(value);
+        printf("Index är %d\n", index);
+        if(index == -1){
+                printf("Inga ettor i datat\n");
+                return 0;
+        }
+        switch(index){
+                case 0 :
+                        return -128;
+                case 1:
+                        return -102;
+                case 2:
+                        return -78;
+                case 3:
+                        return -53;
+                case 4:
+                        return -25;
+                case 5:
+                        return 0;
+                case 6:
+                        return 25;
+                case 7:
+                        return 53;
+                case 8:
+                        return 78;
+                case 9:
+                        return 102;
+                case 10:
+                        return 127;
+                default:
+                        return 0;
+        }
+}
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  get_first_one
+ *  Description:  Funktion som hittar den första ettan från vänster.
+ *                Input: value (11-bitar)
+ *                Output: index på första ettan från vänster. -1 om ingen etta.
+ * =====================================================================================
+ */
+int get_first_one(int value)
+{
+        for(int i=0;i<11;i++){
+                if(value%2 == 1){
+                        return i; 
+                }
+                value = value >> 1;
+        }
+        return -1;
 }
