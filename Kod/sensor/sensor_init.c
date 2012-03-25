cb393089c2417a2d1f5892eb1a9aee0737272b93
Gustav Svensk <grulfen3@gmail.com>
2012-03-25 16:32:10 +0200

    Glömde några filer

---
+++ Kod/sensor/sensor_init.c
@@ -0,0 +1,22 @@
+#include <avr/io.h>
+#include <avr/interrupt.h> 
+
+		
+void spi_init()
+{		
+		DDRB |= (1<<DDB6) | (1<<DDB3); //1=ut bit7=SCLK,1 bit6=MISO,0 bit5=MOSI bit4=SS1
+		SPCR |= (1<<SPIE) | (1<<SPE) | (1<<SPR1) | (1<<SPR0);  
+		//bit7 SPI interrupt enable bit6 SPI enable bit5 LSB first bit4 slave bit3:2 SPI-mode 3, bit1:0 gör inget
+		DDRD |= (1<<DDD2);		//Spak på INT0		
+}
+
+void ad_init()
+{
+		ADMUX |= (1<<ADLAR);				//Vänsterjustering
+		ADCSRA |= (1<<ADEN) | (1<<ADIE) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
+		//Tillåt ADC avbrott, dela frekvens med 128
+		DDRC |= (1<<DDC0) | (1<<DDC1) | (1<<DDC6) | (1<<DDC7);
+		PORTC |= (1<<PC0) | (1<<PC1) | (1<<PC6) | (1<<PC7);
+		TIMSK |= (1<<TOV1);			//Tillåt interrupts på timer overflow
+		TCCR1B |= (1<<CS11);		//Prescaler delar med 8, startar räknare
+}
