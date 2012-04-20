b9c51f5a43ab7a695ddb726f87c6ffe31ec68610
Gustav Svensk <grulfen3@gmail.com>
2012-04-20 18:51:55 +0200

    Fixat specialkommandon

--- Kod/styr/styr_specialkommando.c
+++ Kod/styr/styr_specialkommando.c
@@ -1,25 +1,82 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "styr_SPI.h"
+#include "motor_test.h"
 
 void specialkommando()
 {
-		if(0==data)
+        unsigned char kommando_kod = data & 0xE0;
+
+		if(0x40 == kommando_kod)
 		{
-				/*Sväng vänster 90 grader
-				Förslag: slå på timer och låt roboten köra rakt fram <tid>,
-				rotera vänster <tid>, kör fram <tid>. jag tror det är enklast
-				att använda flaggan för uppfyllt tidskrav här (man väntar på
-				att den är lika med 1)
-				*/
+                        /*Sväng vänster 90 grader
+                        Förslag: slå på timer och låt roboten köra rakt fram <tid>,
+                        rotera vänster <tid>, kör fram <tid>. jag tror det är enklast
+                        att använda flaggan för uppfyllt tidskrav här (man väntar på
+                        att den är lika med 1)
+                        */
+                        OCR1A = 0x3E80;                  // ställer in klockan på 1 sek
+                        TCCR1B = (1<<CS12) | (1<<CS10);  //prescaler på 16-bits klocka till 1024 
+                                                        // och startar klockan
+                        forward(8);
+                        while(!(TIFR & (1<<OCF1A))){    // Vänta tills klockan räknat klart
+                               ;  
+                        }
+                        stop(0);
+
+                        TCCR1B = (0<<CS12) | (0<<CS11) | (0<<CS10); //Stanna klockan
+                        TCNT1 = 0;                      //Nollställ klockan
+                        OCR1A = 0x0FA0;                 // ställer in klockan på 0.25s
+                        TCCR1B = (1<<CS12) | (1<<CS10);  //starta klockan
+                        rotate_left(9);
+                        while(!(TIFR & (1<<OCF1A))){    // Vänta tills klockan räknat klart
+                               ;  
+                        }
+                        stop(0);
+                        OCR1A = 0x3E80;                  // ställer in klockan på 1 sek
+                        TCCR1B = (1<<CS12) | (1<<CS10);  //prescaler på 16-bits klocka till 1024 
+                                                        // och startar klockan
+                        forward(8);
+                        return;
+
 		}
-		else if(0==data)
+		else if(0x60==kommando_kod)
 		{
-				//sväng höger 90 grader
+                        //sväng höger 90 grader
+                        OCR1A = 0x3E80;                  // ställer in klockan på 1 sek
+                        TCCR1B = (1<<CS12) | (1<<CS10);  //prescaler på 16-bits klocka till 1024 
+                                                        // och startar klockan
+                        forward(8);
+                        while(!(TIFR & (1<<OCF1A))){    // Vänta tills klockan räknat klart
+                               ;  
+                        }
+                        stop(0);
+
+                        TCCR1B = (0<<CS12) | (0<<CS11) | (0<<CS10); //Stanna klockan
+                        TCNT1 = 0;                      //Nollställ klockan
+                        OCR1A = 0x0FA0;                 // ställer in klockan på 0.25s
+                        TCCR1B = (1<<CS12) | (1<<CS10);  //starta klockan
+                        rotate_right(9);
+                        while(!(TIFR & (1<<OCF1A))){    // Vänta tills klockan räknat klart
+                               ;  
+                        }
+                        stop(0);
+                        OCR1A = 0x3E80;                  // ställer in klockan på 1 sek
+                        TCCR1B = (1<<CS12) | (1<<CS10);  //prescaler på 16-bits klocka till 1024 
+                                                        // och startar klockan
+                        forward(8);
+                        return;
 		}
-		else if(0==data)
+		else if(0x20==kommando_kod)
 		{
-				//kör rakt fram
+                        //kör rakt fram
+                        OCR1A = 0xBB80;                  // ställer in klockan på 1 sek
+                        TCCR1B = (1<<CS12) | (1<<CS10);  //prescaler på 16-bits klocka till 1024 
+                                                        // och startar klockan
+                        forward(8);
+                        while(!(TIFR & (1<<OCF1A))){    // Vänta tills klockan räknat klart
+                               ;  
+                        }
+                        return;
 		}
 }
-				
