075508ec47e256a079367c2f7a745a8cd32d5cc4
Johan Jönsson <johjo939@student.liu.se>
2012-05-09 16:11:53 +0200

    Gränssnittskoden kommenterad och klar (typ)

--- Kod/pc/display.h
+++ Kod/pc/display.h
@@ -3,6 +3,9 @@
 
 #include "blue_pc.h"
 
+/******************************************************************************
+ * Lägen som Komborobot kan köra i
+ *****************************************************************************/
 enum mode{
 	LABYRINT,
 	LINJE,
@@ -10,6 +13,9 @@ enum mode{
 	ERROR
 };
 
+/*****************************************************************************
+ * Olika typer av data som Komborobot kan skicka
+ *****************************************************************************/
 enum data_type_t{
 	SENSORDATA,
 	REGLERING,
@@ -17,6 +23,9 @@ enum data_type_t{
 	UNKNOWN_DATA
 } data_type;
 
+/******************************************************************************
+ * Specialkommandon som Komborobot kan skicka
+ *****************************************************************************/
 enum spec_komm_t{
 	REGLER,
 	FRAM,
@@ -26,11 +35,36 @@ enum spec_komm_t{
 	UNKNOWN_COMMAND
 };
 
+/******************************************************************************
+ * Initiera det pseudogrfiska gränssnittet
+ * Inparametrar - 
+ * Returvärde   - 
+ *****************************************************************************/
 void init_curses();
 
+/******************************************************************************
+ * Avsluta det pseudografiska gränssnittet och frigör allt minne som allokerats
+ * under initieringen
+ * Inprametrar  - 
+ * Returvärde   -
+ *****************************************************************************/
 void exit_curses();
 
+/******************************************************************************
+ * Uppdatera gränssnittet med aktuell information om önskad hastiget på roboten
+ * Inparametrar - struct instruction_t *inst Instruktionen som skickas till
+ *              Komborobot
+ * Returvärde   - 
+ *****************************************************************************/
 void update_speed(struct instruction_t *inst);
+
+/******************************************************************************
+ * Uppdatera gränssnittet med aktuell information om vad roboten skickar till
+ * datorn
+ * Inparametrar - struct instruction_t *inst Informationen roboten skickat till
+ * datorn
+ * Returvärde   - 
+ *****************************************************************************/
 void display_inst(struct instruction_t *inst); 
 
 
--- Kod/pc/keypress.h
+++ Kod/pc/keypress.h
@@ -1,12 +1,59 @@
 #ifndef KEYPRESS_H
 #define KEYPRESS_H
 
+/******************************************************************************
+ * Generera databyte med kommandot stopp
+ * Inparametrar -
+ * Returvärde   - unsigned char Databyte med kommandot stopp
+ *****************************************************************************/
 extern unsigned char stop();
+
+/******************************************************************************
+ * Generera databyte med kommandot kör framåt, med hastighet speed
+ * Inparametrar - int speed Önskad hastighet på roboten
+ * Returvärde   - unsigned char Databyte med kommandot kör framåt med hastighet
+ * speed
+ *****************************************************************************/
 extern unsigned char forward(int speed);
+
+/******************************************************************************
+ * Generera databyte med kommandot gör en vänstersväng med hastighet speed
+ * Inparametrar - int speed
+ * Returvärde   - unsigned char Databyte med kommandot gör en vänstersväng med
+ *              hastighet speed
+ *****************************************************************************/
 extern unsigned char forward_left(int speed);
+
+/******************************************************************************
+ * Generera databyte med kommandot gör en högerersväng med hastighet speed
+ * Inparametrar - int speed
+ * Returvärde   - unsigned char Databyte med kommandot gör en högersväng med
+ *              hastighet speed
+ *****************************************************************************/
 extern unsigned char forward_right(int speed);
+
+/******************************************************************************
+ * Generera databyte med kommandot rotera vänster med hastighet speed
+ * Inparametrar - int speed
+ * Returvärde   - unsigned char Databyte med kommandot rotera vänster  med 
+ * hastighet speed
+ *****************************************************************************/
 extern unsigned char rotate_left(int speed);
+
+/******************************************************************************
+ * Generera databyte med kommandot rotera höger med hastighet speed
+ * Inparametrar - int speed
+ * Returvärde   - unsigned char Databyte med kommandot rotera höger  med 
+ * hastighet speed
+ *****************************************************************************/
 extern unsigned char rotate_right(int speed);
+
+/******************************************************************************
+ * Generera databyte med kommandot kör bakåt med hastighet speed
+ * Inparametrar - int speed
+ * Returvärde   - unsigned char Databyte med kommandot kör bakåt med
+ * hastighet speed
+ *****************************************************************************/
 extern unsigned char reverse(int speed);
 
 #endif
